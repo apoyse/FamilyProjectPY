fdcd2d4219f6a0f3c0e0c6d28716cc97e2db5d58
Andres.poy <apoysegu@gmail.com>
2022-04-03 11:58:57 -0300

    Proyecto ya terminado

--- aplicacion/models.py
+++ aplicacion/models.py
@@ -5,9 +5,10 @@ from django.db import models
 class Familiar(models.Model):
     name = models.CharField(max_length=40)
     apellido = models.CharField(max_length=40)
-    tipofamilia = models.CharField( max_length=50)
+    rol_familiar = models.CharField( max_length=50)
     edad = models.IntegerField(null=True)
     fecha_nacimiento = models.DateField(null=True)
     id = models.AutoField(primary_key=True)
-
+    def __str__(self):
+        return f"{self.name} {self.apellido} "
 # Create your models here.
