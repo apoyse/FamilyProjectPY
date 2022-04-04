c8fed9167c287170ef6d58511ce3123f7422b26b
Andres Poysegu <apoysegu@gmail.com>
2022-04-04 16:11:54 -0300

    Update views.py

--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,12 +1,6 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 from aplicacion.models import Familiar
-
-def familares(request):
-    return HttpResponse("vista de estudiantes")
-
-def add(request):
-    return render(request, 'aplicacion/add.html')
 
 def page(request):
     familiares = Familiar.objects.all()
